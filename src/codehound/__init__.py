2cea6b044b116f4934b9b49ea4622ea5bc88184e
Abhinav Tarigoppula <abhinaaavvv07187@gmail.com>
2026-09-17 08:07:56 +0530

    Bump version to 1.3.0

--- src/codehound/__init__.py
+++ src/codehound/__init__.py
@@ -11,7 +11,7 @@ from __future__ import annotations
 from codehound.checks import ALL_CHECKS, get_checks
 from codehound.core import Check, Finding, scan_file, scan_path
 
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 
 __all__ = [
     "ALL_CHECKS",
